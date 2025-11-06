b78a5c38b94b3d58638493f49c64d722b6c16dd5
WiktoriaSwiatkowska <243307@student.uek.krakow.pl>
2025-11-06 19:58:37 +0100

    task

---
+++ 04-Functions/task2.py
@@ -0,0 +1,4 @@
+import math
+
+square_root = math.sqrt(7)
+print('A square root of 7 is', square_root)
---
+++ 04-Functions/test.py
@@ -0,0 +1,36 @@
+max_number = max(7,5,6,3,8,2)
+print('Max number of 7,5,6,3,8,2 is', max_number)
+
+min_number = min(4,7,2,3,9,8)
+print('Min number of 4,7,2,3,9,8 is', min_number)
+
+str_length = len("computer science")
+print('The number of characters in "computer science" is', str_length)
+
+text= input("write")
+print(text)
+
+number= int(20303)
+print(number)
+
+binary= bin(304)
+print(binary)
+
+decimal= hex(304)
+print(decimal)
+
+unicode = ord(8364)
+print(unicode)
+
+absolute=abs(-17)
+print(absolute)
+
+
+
+
+
+
+
+
+
+
