6fec605d244beafe9c5f5520a0868fb4ae400ce3
WiktoriaSwiatkowska <243307@student.uek.krakow.pl>
2025-12-04 19:52:09 +0100

    tasksss

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -6,4 +6,16 @@ class SocialMediaProfile:
     def add_post(self, content):
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
+    
+    def display_timeline(self):
+        print(self.username)
+        print[self.posts]
 
+def main():
+    john= SocialMediaProfile("johndoe")
+    john.add_post["Hello, world!","Had a great day at the park!","What's up, Natalie? How are you?"]
+    john.display_timeline()
+
+
+if __name__ =="__main__":
+    main()
--- 11-ObjectOrientedProgramming/taxi.py
+++ 11-ObjectOrientedProgramming/taxi.py
@@ -8,12 +8,25 @@ class TaxiRide:
         self.distance = distance
         self.fare = self.distance * self.rate_per_km
 
+    def print_recipt(self):
+        print(f"the distance:{self.distance}")
+        print(f'the fare:{self.fare}')
+        print(f'the rate:{self.rate_per_km}')
+        
+
+
 
 def main():
     # your program
-    ...
-    ...
-    ...
+    taxi1 =  TaxiRide(3)
+    taxi1.distance = 4
+    taxi1.calculate_fare(4)
+    taxi1.print_recipt()
+
+    taxi2 =  TaxiRide(6)
+    taxi2.distance = 7
+    taxi2.calculate_fare(7)
+    taxi2.print_recipt()
 
 if __name__ == "__main__":
     main()
